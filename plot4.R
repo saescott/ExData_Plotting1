d47008b5eb897e17e2b43e4bf40161b0015b27da
saescott <saescott@gmail.com>
2017-07-11 13:27:25 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -51,3 +51,7 @@ legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1),
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 plot(hpc2$Global_reactive_power~hpc2$date.time, type="l", 
      ylab="Global_reactive_power",xlab="datetime")
+
+## Create .png file 
+dev.copy(png, file="plot4.png", width=480, height=480)
+dev.off()
